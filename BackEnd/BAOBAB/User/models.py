0272f717041161e2dc2581233ab8dc2753211b2a
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-07-29 23:32:22 +0900

    feat : add has_perm

--- BackEnd/BAOBAB/User/models.py
+++ BackEnd/BAOBAB/User/models.py
@@ -18,8 +18,8 @@ class User(AbstractBaseUser, TimestampedModel):
     email = models.EmailField(max_length=50, unique=True)
     nickname = models.CharField(max_length=50, unique=True)
     
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    # is_active = models.BooleanField(default=True)
     
     # 로그인시 id로 사용할 필드 지정
     USERNAME_FIELD = 'email'
@@ -27,6 +27,12 @@ class User(AbstractBaseUser, TimestampedModel):
     REQUIRED_FIELDS = ['username', 'nickname']
     
     object = UserManager()
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
     
     def __str__(self):
         return self.username
