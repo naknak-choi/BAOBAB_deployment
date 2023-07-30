c01dfe4f58eef2ce2a22d844f4231a44eee2f392
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-07-30 22:02:01 +0900

    feat : add Create Category

--- BackEnd/BAOBAB/Category/models.py
+++ BackEnd/BAOBAB/Category/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, primary_key=True, unique=True)
-    mainCategory_name = models.OneToOneField('self', on_delete=models.CASCADE, unique=True, null=True, blank=True)
+    mainCategory_name = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     is_main = models.BooleanField(default=False)
     
     created_at = models.DateTimeField(auto_now_add=True)
--- BackEnd/BAOBAB/Category/views.py
+++ BackEnd/BAOBAB/Category/views.py
@@ -15,15 +15,18 @@ class CreateCategoryView(generics.CreateAPIView):
         serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             is_main = data.get('is_main')
-            main_category = data.get('main_category')
+            mainCategory_name = data.get('mainCategory_name')
             
+            # print(mainCategory_name == "")
+            # print("is_main : " + is_main)
             if is_main:
-                if main_category is not None:
+                if not mainCategory_name == "":
                     return Response({'error': '메인카테고리는 하위카테고리로 설정할 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
             else:
-                if main_category is None:
+                if mainCategory_name == "":
                     return Response({'error': '메인카테고리를 지정해주세요'}, status=status.HTTP_400_BAD_REQUEST)
-                elif not Category.objects.filter(pk=main_category).exists():
+                
+                elif not Category.objects.filter(pk=mainCategory_name).exists():
                     return Response({'error': '메인카테고리가 존재하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
                 
             serializer.save()
