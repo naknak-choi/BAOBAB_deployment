41a3cc0d0f1710004ebfa4f868222a20c73b154a
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-08-04 18:50:58 +0900

    merge : User & other models

--- BackEnd/BAOBAB/User/urls.py
+++ BackEnd/BAOBAB/User/urls.py
@@ -7,8 +7,8 @@ user_detail = views.UserViewSets.as_view({
 })
 
 urlpatterns = [
-    path('register', RegistrationAPIView.as_view()),
-    path('login', LoginAPIView.as_view()),
-    path('current', UserRetrieveUpdateAPIView.as_view()),
+    # path('register', RegistrationAPIView.as_view()),
+    # path('login', LoginAPIView.as_view()),
+    # path('current', UserRetrieveUpdateAPIView.as_view()),
     path('detail/<int:id>', user_detail, name='user_detail'),
 ]
