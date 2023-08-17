fd6bbcf7f3227081b40ec4a4195f498dcc29de88
naknak <9naknak8@gmail.com>
2023-08-18 07:02:37 +0900

    1

--- BackEnd/BAOBAB/BAOBAB/settings.py
+++ BackEnd/BAOBAB/BAOBAB/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_value("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.211.227.97', 'baobab.kro.kr']
 
 
 # Application definition
@@ -105,7 +105,7 @@ ROOT_URLCONF = "BAOBAB.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "frontend_build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -168,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend_build"),
 ]
@@ -239,4 +239,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "13.211.227.97",
+    'baobab.kro.kr',
 ]
--- BackEnd/BAOBAB/BAOBAB/urls.py
+++ BackEnd/BAOBAB/BAOBAB/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
+from django.urls import re_path
 urlpatterns = [
     path("admin/", admin.site.urls),
     
@@ -8,4 +9,8 @@ urlpatterns = [
     path("category/", include("Category.urls")),
     path('comment/', include('Comment.urls')),
     path('user/', include('User.urls')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    
 ]
+
+
