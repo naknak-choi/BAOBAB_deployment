edb6b0a6fc6fa87814839c1821d0a12c7b4c43a0
naknak <9naknak8@gmail.com>
2023-08-18 04:02:15 +0900

    add build

--- BackEnd/BAOBAB/BAOBAB/settings.py
+++ BackEnd/BAOBAB/BAOBAB/settings.py
@@ -169,6 +169,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend_build"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
